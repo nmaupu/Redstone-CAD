a0cf78104b349480ac583ad1f77542c2fbfc69b0
mgrenonville <mgrenonville@excilys.com>
2011-03-30 17:51:51 +0200

    lever

--- rcad/src/main/java/net/fossar/core/block/Lever.java
+++ rcad/src/main/java/net/fossar/core/block/Lever.java
@@ -6,31 +6,27 @@ import java.util.Map;
 
 public class Lever extends AbstractBlock {
 
-	public Lever() {
-		super(POWER_OFF, 0);
-	}
-	
-	public void powerOn() {
-		super.setInput(POWER_SOURCE);
-	}
-	
-	public void powerOff() {
-		super.setInput(POWER_OFF);
-	}
-	
-	@Override
-	public void doUpdate(Map<Direction, AbstractBlock> adjacentBlocks) {
+    public Lever() {
+        super(POWER_OFF, 0);
+    }
 
+    public void powerOn() {
+        super.setInput(POWER_SOURCE);
+    }
+
+    public void powerOff() {
+        super.setInput(POWER_OFF);
+    }
+
+    @Override
+    public void doUpdate(Map<Direction, AbstractBlock> adjacentBlocks) {
         for (Map.Entry<Direction, AbstractBlock> entry : adjacentBlocks.entrySet()) {
             AbstractBlock block = entry.getValue();
-            block.setInput(getOutput());
-            if(block instanceof Wire){
-
+            if (block instanceof Wire) {
+                block.setInput(getOutput());
+//                block.doUpdate(adjacentBlocks.getAdjacents(block));
             }
-            
-            
         }
-
-	}
+    }
 
 }
