5fb0554800ff1c5cec3a3565ae8b3c6110942ebf
mgrenonville <mgrenonville@excilys.com>
2011-03-30 23:26:20 +0200

    implement logic for torch... maybe a little simple...

--- rcad/src/main/java/net/fossar/core/block/Torch.java
+++ rcad/src/main/java/net/fossar/core/block/Torch.java
@@ -1,11 +1,15 @@
 package net.fossar.core.block;
 
+import java.util.Map;
+
 import net.fossar.core.AdjacentBlocks;
+import net.fossar.core.Direction;
 
 public class Torch extends AbstractBlock implements ActiveBlock {
 
 	public Torch() {
 		super(POWER_SOURCE, 0);
+		powerOn();
 	}
 
 	@Override
@@ -20,7 +24,16 @@ public class Torch extends AbstractBlock implements ActiveBlock {
 
 	@Override
 	public void doUpdate(AdjacentBlocks adjacentBlocks) {
-		// TODO Auto-generated method stub
+		for (Map.Entry<Direction, AbstractBlock> entry : adjacentBlocks.entrySet()) {
+			AbstractBlock block = entry.getValue();
+			if (block instanceof Block) {
+				if (block.isPowered()) {
+					this.powerOff();
+				} else {
+					this.powerOn();
+				}
+			}
+		}
 
 	}
 }
