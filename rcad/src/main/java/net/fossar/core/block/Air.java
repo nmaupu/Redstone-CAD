d262175be31f179aa0cb897bd454d6a6a07218e7
mgrenonville <mgrenonville@excilys.com>
2011-03-30 23:25:53 +0200

    adding Air block as an immutable singleton

---
+++ rcad/src/main/java/net/fossar/core/block/Air.java
@@ -0,0 +1,54 @@
+package net.fossar.core.block;
+
+import net.fossar.core.AdjacentBlocks;
+import net.fossar.core.Direction;
+
+public class Air extends AbstractBlock {
+	public static final Air INSTANCE = new Air();
+
+	protected Air() {
+		super(-1, -1);
+	}
+
+	@Override
+	public void addBlockPowerListener(BlockPowerListener listener) {
+		return;
+	}
+
+	@Override
+	public Direction getDirection() {
+		return Direction.UNDEF;
+	}
+
+	@Override
+	public int getOutput() {
+		return 0;
+	}
+
+	@Override
+	public boolean isPowered() {
+		return false;
+	}
+
+	@Override
+	public boolean isUnpowered() {
+		return true;
+	}
+
+	@Override
+	public void setDirection(Direction direction) {
+		return;
+	}
+
+	@Override
+	public void setInput(int power) {
+		return;
+	}
+
+	@Override
+	public void doUpdate(AdjacentBlocks adjacentBlocks) {
+		return;
+
+	}
+
+}
