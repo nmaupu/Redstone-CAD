ac3999a3656087846900b50135b58d2a7df47f07
Nicolas Maupu <nmaupu@gmail.com>
2011-04-01 23:35:14 +0200

    License update

--- rcad/src/test/java/net/fossar/core/clock/ClockTest.java
+++ rcad/src/test/java/net/fossar/core/clock/ClockTest.java
@@ -1,3 +1,20 @@
+/*
+ * RCad is a software to help manipulating minecraft's redstone.
+ * Copyright (C) 2011. mathieu_dot_grenonville_at_gmail_dot_com - nmaupu_at_gmail_dot_com
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.fossar.core.clock;
 
 import java.util.concurrent.locks.Lock;
